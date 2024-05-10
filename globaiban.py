55888cc88168c41aa7b13a5fa4d3a8ab24e9cc3d
green <41323182+greeeen-dev@users.noreply.github.com>
2024-05-10 17:42:14 +0200

    Update globaiban.py

--- globaiban.py
+++ globaiban.py
@@ -18,6 +18,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import discord
 from discord.ext import commands
+import time
 
 class GlobaiBan(commands.Cog):
     """we do a lil trolling"""
